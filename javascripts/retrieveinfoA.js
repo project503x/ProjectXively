235aa499641c4a973e73732ad708e12e9ba66464
project503x <project503x@gmail.com>
2014-11-27 09:53:49 +0800

    Update and rename newretrieveinfo.js to retrieveinfoA.js

--- javascripts/retrieveinfoA.js
+++ javascripts/retrieveinfoA.js
@@ -3,13 +3,10 @@ $(document).ready(function($) {
 
   // Set the Xively API key (https://xively.com/users/YOUR_USERNAME/keys)
   xively.setKey( "A4lGfHsBnd3sTzbltW9J6SgkHH4pzcQxlf7zGAo1qxlS1Wkc" );
-  //xively.setKeyB( "rQ6YvuNELKLR8Wj0RkblfS3UwM7Q5Vf35d5Ond6I34FKabEk" );
   
   // Replace with your own values
   var feedA        = 1260369820;        // Feed ID (the last number on the URL on the feed page on Xively)
-      //feedB        = 483265714;
   
-  // feedA
   xively.feed.get (feedA, function (data) {
     $("#titleA").html( data["title"] );
     xively.feed.subscribe( feedA, function ( event , feed_updated ) {
@@ -44,44 +41,6 @@ $(document).ready(function($) {
       $("#sleepingquarterA").html( datastream_updated["current_value"] );
     });
   });
-  
-  // feedB
-  /*
-  xively.feed.get (feedB, function (data) {
-    $("#titleB").html( data["title"] );
-    xively.feed.subscribe( feedB, function ( event , feed_updated ) {
-      $("#titleB").html( feed_updated["title"] );
-    });
-  });
-  
-  xively.datastream.get (feedB, "AssemblyArea", function (datastream) {
-    $("#assemblyareaB").html( datastream["current_value"] );
-    xively.datastream.subscribe( feedB, "AssemblyArea", function ( event , datastream_updated ) {
-      $("#assemblyareaB").html( datastream_updated["current_value"] );
-    });
-  });
-  
-  xively.datastream.get (feedB, "MedicalDispensary", function (datastream) {
-    $("#medicaldispensaryB").html( datastream["current_value"] );
-    xively.datastream.subscribe( feedB, "MedicalDispensary", function ( event , datastream_updated ) {
-      $("#medicaldispensaryB").html( datastream_updated["current_value"] );
-    });
-  });
-  
-  xively.datastream.get (feedB, "ReceptionOffice", function (datastream) {
-    $("#receptionofficeB").html( datastream["current_value"] );
-    xively.datastream.subscribe( feedB, "ReceptionOffice", function ( event , datastream_updated ) {
-      $("#receptionofficeB").html( datastream_updated["current_value"] );
-    });
-  });
-
-  xively.datastream.get (feedB, "SleepingQuarter", function (datastream) {
-    $("#sleepingquarterB").html( datastream["current_value"] );
-    xively.datastream.subscribe( feedB, "SleepingQuarter", function ( event , datastream_updated ) {
-      $("#sleepingquarterB").html( datastream_updated["current_value"] );
-    });
-  });
-  */
   
   // WARNING: Code here will continue executing while we get the datastream data from Xively,
   // use the function associated with datastream.get to work with the data
